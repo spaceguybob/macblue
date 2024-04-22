47c19a46c9ede354497ef81a7008a5c330f6dd32
SpaceGuyBob <92336100+spaceguybob@users.noreply.github.com>
2024-04-22 14:38:59 -0400

    Create tela.sh

---
+++ config/scripts/tela.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
+cd Tela-circle-icon-theme
+ ./install.sh -a -c
